796dd96d909a3836ce37b7aca66c8033578415f6
Po <poc2@student.unimelb.edu.au>
2018-12-08 23:29:07 +1100

    add XMLHttpRequest

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 console.log('serviceWorker work');
 
 self.addEventListener('push', function(event) {
@@ -19,6 +20,21 @@ self.addEventListener('push', function(event) {
 self.addEventListener('notificationclick', function(event) {
   console.log("click!!!");
   event.notification.close();
+
+  const request = new XMLHttpRequest();
+  const url = 'https://ax7asfdg4f.execute-api.ap-southeast-2.amazonaws.com/prod/clicked';
+  request.open("POST", url, true);
+  
+  request.onreadystatechange = function() { // Call a function when the state changes.
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        // Request finished. Do processing here.
+        console.log("Clicked campaign updated");
+    }
+  }
+
+  if (event.notification && event.notification.eventId) {
+    request.send(JSON.stringify({ Guid: eventId }));
+  }
   
   let clickResponsePromise = Promise.resolve();
   if (event.notification.data && event.notification.data.url) {
