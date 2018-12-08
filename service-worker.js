efdb52e0105cba1ca5e1259d0cf51a2ae74ab790
Po <poc2@student.unimelb.edu.au>
2018-12-08 21:14:59 +1100

    Update click event

--- service-worker.js
+++ service-worker.js
@@ -20,9 +20,24 @@ self.addEventListener('notificationclick', function(event) {
   console.log("click!!!");
   event.notification.close();
 
+  const request = new XMLHttpRequest();
+  const url = 'https://ax7asfdg4f.execute-api.ap-southeast-2.amazonaws.com/prod/clicked';
+  request.open("POST", url, true);
+  
+  request.onreadystatechange = function() { // Call a function when the state changes.
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        // Request finished. Do processing here.
+        console.log("Clicked campaign updated");
+    }
+  }
+  
   let clickResponsePromise = Promise.resolve();
-  if (event.notification.data && event.notification.data.url) {
-    clickResponsePromise = clients.openWindow(event.notification.data.url);
+  if (event.notification && event.notification.url) {
+    clickResponsePromise = clients.openWindow(event.notification.url);
+  }
+
+  if (event.notification && event.notification.eventId) {
+    request.send(JSON.stringify({ Guid: eventId }));
   }
 
   event.waitUntil(
